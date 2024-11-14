0736e7c8f240382e6dc9b48ea4fe3bed923ae7d4
saascart <support@saasc.art>
2024-11-14 15:48:00 +1100

    Update co.blade.php

--- src/PhUntVw/co.blade.php
+++ src/PhUntVw/co.blade.php
@@ -4,7 +4,7 @@
 <div class="wizard-step-4 d-block">
     <div class="install-complete">
         <i data-feather="check-circle"></i>
-        <h3>API is running successfully.</h3>
+        <h3>API is running successfully</h3>
     </div>
 </div>
 <div class="row goto-selection">
